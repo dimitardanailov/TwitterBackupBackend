ad2f3b82bc744d63895735fce6d8de3230e67652
Dimitar Danailov <dimityr.danailov@gmail.com>
2015-09-29 18:08:33 +0000

    Add Project Setup. Add Project for Models and Project for Repositories

---
+++ README.md
@@ -0,0 +1,23 @@
+# Twitter Backup
+
+### Table of Contents
+  1. [Twitter](#twitter)
+  1. 
+
+### Twitter
+
+##### Microsoft Owin
+
+> Middleware that enables an application to support Twitter's OAuth 2.0 authentication workflow.
+
+We use version: 3.0.1
+
+```bash
+PM> Install-Package Microsoft.Owin.Security.Twitter
+```
+
+### Sources:
+  1. [Microsoft Owin Twitter](https://www.nuget.org/packages/Microsoft.Owin.Security.Twitter/)
+  1. [Code! MVC 5 App with Facebook, Twitter, LinkedIn and Google OAuth2 Sign-on (C#) | The ASP.NET Site](http://www.asp.net/mvc/overview/security/create-an-aspnet-mvc-5-app-with-facebook-and-google-oauth2-and-openid-sign-on)
+  
+**[⬆ back to top](#table-of-contents)**
---
+++ TwitterBackupEntities.Repositories/IRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterBackupEntities.Repositories
+{
+    /// <summary>
+    /// We will use IRepository to describe main database queries
+    /// 
+    /// <see cref="http://blog.sapiensworks.com/post/2012/03/05/The-Generic-Repository-Is-An-Anti-Pattern.aspx/"/>
+    /// <see cref="https://irepository.codeplex.com/"/>
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    interface IRepository<T> where T : class
+    {
+        /// <summary>
+        /// Get all records
+        /// </summary>
+        /// <returns></returns>
+        IQueryable<T> GetAll();
+
+        /// <summary>
+        /// Get Record by record id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        T GetById(int id);
+
+        /// <summary>
+        /// Create a new Item
+        /// </summary>
+        /// <param name="item"></param>
+        void Create(T item);
+
+        /// <summary>
+        /// Update a record
+        /// </summary>
+        /// <param name="entity"></param>
+        void Update(T entity);
+
+        /// <summary>
+        /// Delete item by Entity reference
+        /// </summary>
+        /// <param name="entity"></param>
+        void Delete(T entity);
+
+        /// <summary>
+        /// Delete item by id
+        /// </summary>
+        /// <param name="id"></param>
+        void Delete(int id);
+    }
+}
